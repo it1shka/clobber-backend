82544dc7f96a30ce0bfaf2088e03a2f4cd2e01ef
it1shka <2tishbel@gmail.com>
2025-05-09 17:21:46 +0200

    feat: added CORS

--- src/api.zig
+++ src/api.zig
@@ -3,6 +3,15 @@ const zap = @import("zap");
 const handlers = @import("handlers.zig");
 
 fn handleRequest(r: zap.Request) !void {
+    // CORS
+    if (std.mem.eql(u8, r.method orelse "", "OPTIONS")) {
+        r.setHeader("Access-Control-Allow-Origin", "http://localhost:5173") catch return;
+        r.setHeader("Access-Control-Allow-Headers", "Content-Type") catch return;
+        r.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS") catch return;
+        r.setStatus(.ok);
+        return;
+    }
+
     normal_flow: {
         r.setHeader("Content-Type", "application/json") catch break :normal_flow;
         if (r.path) |path| {
